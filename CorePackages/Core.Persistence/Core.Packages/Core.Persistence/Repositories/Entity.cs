e919e52f4f34263f4e0c251ec0402dc88b464e31
ugur <ercinugur@gmail.com>
2024-07-01 21:38:41 +0300

    BudgetCars(CarRental) .NET project backbone

---
+++ CorePackages/Core.Persistence/Core.Packages/Core.Persistence/Repositories/Entity.cs
@@ -0,0 +1,22 @@
+﻿namespace Core.Persistence.Repositories
+{
+    public class Entity<T>
+    {
+        public T Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+
+        public Entity()
+        {
+            Id = default;
+        }
+
+        public Entity(T id)
+        {
+            Id = id;
+        }
+    }
+}
+
+
